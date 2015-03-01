07934a09ce0f3a92f4d69cdb9e69490919b402f1
Ryan Van Etten <ryanve@gmail.com>
2015-03-01 13:15:00 -0800

    Move bulk attr() functionality into separate atts() method

--- src/index.js
+++ src/index.js
@@ -5,11 +5,10 @@
 
   var ssv = /\S+/g
     , effin = api.prototype
-    , owns = {}.hasOwnProperty
     , setAttr = 'setAttribute'
     , getAttr = 'getAttribute'
     , remAttr = 'removeAttribute'
-    , unbox = 'valueOf';
+    , owns = {}.hasOwnProperty;
     
   /**
    * @constructor
@@ -49,20 +48,32 @@
   function setAtts(e, o) {
     for (var n in o) owns.call(o, n) && attr(e, n, o[n]);
   }
-  
+
   /**
-   * @param {Element} e element
-   * @param {(string|String|Object)=} k attribute name
+   * @param {Element} e
+   * @param {Object=} o
+   */
+  function atts(e, o) {
+    return void 0 === o ? getAtts(e) : setAtts(e, o);
+  }
+
+  /**
+   * @param {*} v
+   * @return {string|undefined}
+   */
+  function normalize(v) {
+    return null == v ? void 0 : '' + v;
+  }
+
+  /**
+   * @param {Element} e
+   * @param {string=} k attribute name
    * @param {(string|boolean|null)=} v attribute value
    */  
   function attr(e, k, v) {
-    if (1 !== e.nodeType) throw new TypeError('@0');
-    if (void 0 === k && k === v) return getAtts(e);
-    k = k[unbox](); // throws on null
-    if (typeof k != 'string') setAtts(e, k);
-    else if (void 0 === v) return null == (k = e[getAttr](k)) ? v : '' + k;
+    if (void 0 === v) return normalize(e[getAttr](k));
+    if (typeof v == 'boolean') toggleAttr(e, k, v);
     else if (null === v) e[remAttr](k);
-    else if (typeof v == 'boolean') toggleAttr(e, k, v);
     else e[setAttr](k, v = '' + v);
     return v;
   }
@@ -122,7 +133,7 @@
   }
   
   api['attr'] = attr;
-  api['atts'] = getAtts;
+  api['atts'] = atts;
   api['isAttr'] = isAttr;
   api['supportAttr'] = supportAttr;
   api['anyAttr'] = anyAttr;
@@ -131,14 +142,24 @@
   
   /**
    * @this {{length:number}}
-   * @param {(string|Object)=} k
+   * @param {Object=} o
+   */  
+  effin['atts'] = function(o) {
+    return void 0 === o ? atts(this[0]) : each(this, function(e) {
+      atts(e, o);
+    });
+  };
+  
+  /**
+   * @this {{length:number}}
+   * @param {string=} k
    * @param {*=} v
    */  
   effin['attr'] = function(k, v) {
-    return void 0 !== v || k !== v && typeof(k = k[unbox]()) != 'string' ? each(this, function(e) {
+    return void 0 === v ? attr(this[0], k) : each(this, function(e) {
       var x = typeof v == 'function' ? v.call(e) : v;
       void 0 === x || attr(e, k, x);
-    }) : attr(this[0], k);
+    });
   };
   
   /**
--- test/index.js
+++ test/index.js
@@ -19,24 +19,19 @@
     })
       
     keys && it('has prototype methods', function() {
-      var meths = ['attr', 'toggleAttr', 'removeAttr']
+      var meths = ['atts', 'attr', 'toggleAttr', 'removeAttr']
       expect(keys(api.prototype).sort().join()).to.equal(meths.sort().join())
     })
   })
     
-  if (!doc) return // browser tests:
+  // remaining tests need a browser
+  if (!doc) return
   
-  describe('.attr', function() {
+  describe('#attr', function() {
     it('sets and gets', function() {
       var k = 'id', v = 'a', e = doc.createElement('div')
       api.attr(e, k, v)
       return expect(api.attr(e, k)).to.equal(v)
-    })
-    
-    it('gets and sets all', function() {
-      var e = doc.createElement('div'), o = {'id': 'a', 'data-a': 'a'}
-      api.attr(e, o)
-      return expect(api.attr(e)).to.deep.equal(o)
     })
     
     it('normalizes to string', function() {
@@ -50,7 +45,15 @@
     })
   })
   
-  describe('.removeAttr', function() {
+  describe('#atts', function() {
+    it('gets and sets all', function() {
+      var e = doc.createElement('div'), o = {'id': 'a', 'data-a': 'a'}
+      api.atts(e, o)
+      return expect(api.atts(e)).to.deep.equal(o)
+    })
+  })
+  
+  describe('#removeAttr', function() {
     it('removes', function() {
       var k = 'id', e = doc.createElement('div')
       e.setAttribute(k, 'a')
@@ -59,7 +62,7 @@
     })
   })
   
-  describe('.toggleAttr', function() {
+  describe('#toggleAttr', function() {
     it('toggles on', function() {
       var k = 'data-a', e = doc.createElement('div')
       api.toggleAttr(e, k)
@@ -72,9 +75,17 @@
       api.toggleAttr(e, k)
       expect(e.getAttribute(k)).to.not.exist
     })
+    
+    it('toggles with force', function() {
+      var k = 'data-a', e = doc.createElement('div')
+      api.toggleAttr(e, k, true)
+      expect(e.getAttribute(k)).to.equal('')
+      api.toggleAttr(e, k, false)
+      expect(e.getAttribute(k)).to.not.exist
+    })
   })
   
-  describe('.isAttr', function() {
+  describe('#isAttr', function() {
     it('detects attr state', function() {
       var custom = 'data-custom', e = doc.createElement('div')
       expect(api.isAttr(e, custom)).to.equal(false)
@@ -83,7 +94,7 @@
     })
   })
   
-  describe('.supportAttr', function() {
+  describe('#supportAttr', function() {
     it('detects [checked] support', function() {
       var input = doc.createElement('input')
       expect(api.supportAttr(input, 'checked')).to.equal('checked' in input)
@@ -102,6 +113,81 @@
     it('detects [data-*] support false', function() {
       var e = doc.createElement('div')
       expect(api.supportAttr(e, 'data-arbitrary')).to.equal(false)
+    })
+  })
+  
+  // remaining tests aren't needed in old browsers
+  if (!keys) return
+  
+  describe('.attr', function() {
+    it('sets and gets', function() {
+      var k = 'id', v = 'a', e = doc.createElement('div')
+      return expect(api(e).attr(k, v).attr(k)).to.equal(v)
+    })
+    
+    it('sets via function', function() {
+      var k = 'id', v = 'a', e = doc.createElement('div')
+      return expect(api(e).attr(k, function() {
+        return v;
+      }).attr(k)).to.equal(v)
+    })
+    
+    it('sets for each', function() {
+      var k = 'class', v = 'test', stack = [doc.createElement('div'), doc.createElement('div')]
+      api.prototype.attr.call(stack, k, v)
+      expect(api.attr(stack[0], k)).to.equal(v)
+      expect(api.attr(stack[1], k)).to.equal(v)
+    })
+  })
+  
+  describe('.atts', function() {
+    it('gets and sets all', function() {
+      var e = doc.createElement('div'), o = {'id': 'a', 'data-a': 'a'}
+      return expect(api(e).atts(o).atts()).to.deep.equal(o)
+    })
+  })
+  
+  describe('.removeAttr', function() {
+    function check(stack) {
+      stack.forEach(function(e) {
+        list.forEach(function(k) {
+          expect(api.attr(e, hash[k])).to.be.undefined
+        })
+      })
+    }
+    var hash = {'data-custom': '', 'hidden': ''}
+    var list = keys(hash)
+
+    it('removes ssv for each', function() {
+      var stack = [doc.createElement('div'), doc.createElement('div')]
+      api.prototype.atts.call(stack, hash)
+      api.prototype.removeAttr.call(stack, keys(hash).join(' '))
+      check(stack)
+    })
+    
+    it('removes array for each', function() {
+      var stack = [doc.createElement('div'), doc.createElement('div')]
+      api.prototype.atts.call(stack, hash)
+      api.prototype.removeAttr.call(stack, keys(hash))
+      check(stack)
+    })
+  })
+  
+  describe('.toggleAttr', function() {
+    it('toggles for each', function() {
+      var stack = [doc.createElement('div'), doc.createElement('div')]
+      var k = 'hidden'
+      api.prototype.attr.call(stack, k, '')
+      api.prototype.toggleAttr.call(stack, k)
+      stack.forEach(function(e) {
+        expect(api.attr(e, k)).to.be.undefined
+      })
+    })
+    
+    it('toggles with force', function() {
+      var e = doc.createElement('div')
+      var k = 'hidden'
+      expect(api(e).attr(k, '').toggleAttr(k, false).attr(k)).to.be.undefined
     })
   })
 }(this);
